b2de8b97c0da8e5c4fc0bfd20012e5aa6b938b87
fengzhenzhou <416422890@qq.com>
2016-11-13 20:40:19 +0800

    qq

--- src/main/java/controller/Login.java
+++ src/main/java/controller/Login.java
@@ -1,8 +1,12 @@
 package controller;
 
 public class Login {
-public static void main(String[] args) {
+	public static void main(String[] args) {
+	System.out.println("success");
 	
+	System.out.println("success");
+	System.out.println("success");
+	System.out.println("success");
 	
 }
 }
