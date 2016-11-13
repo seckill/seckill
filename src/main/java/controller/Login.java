1e275959a3def8b215580efcfb636208067d4c61
fengzhenzhou <416422890@qq.com>
2016-11-13 20:30:47 +0800

    222

--- src/main/java/controller/Login.java
+++ src/main/java/controller/Login.java
@@ -9,5 +9,7 @@ public static void main(String[] args) {
 	System.out.println("success");
 	System.out.println("success");
 	System.out.println("success");
+	System.out.println("success");
+	System.out.println("success");
 }
 }
