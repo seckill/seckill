353b3eda54b8486e83aab87422319bf45e0b20a4
wanghongchao <wanghongchao@aikxian.com>
2016-11-13 20:27:09 +0800

    11

--- src/main/java/controller/Login.java
+++ src/main/java/controller/Login.java
@@ -5,5 +5,9 @@ public static void main(String[] args) {
 	System.out.println("success");
 	
 	System.out.println("success");
+	System.out.println("success");
+	System.out.println("success");
+	System.out.println("success");
+	System.out.println("success");
 }
 }
